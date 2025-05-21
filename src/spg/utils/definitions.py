54283d2f433137b3c69060c08fc4cf84c0df6b42
Emmanuel Battesti <emmanuel.battesti@ensta-paris.fr>
2025-05-21 19:19:21 +0200

    For place-bot, set SPACE_DAMPING from 0.95 to 0.8

--- src/spg/utils/definitions.py
+++ src/spg/utils/definitions.py
@@ -6,7 +6,8 @@ from collections import namedtuple
 from enum import IntEnum, auto
 
 SIMULATION_STEPS = 10
-SPACE_DAMPING = 0.95 # https://www.pymunk.org/en/latest/pymunk.html#pymunk.Space.damping
+SPACE_DAMPING = 0.8
+# https://www.pymunk.org/en/latest/pymunk.html#pymunk.Space.damping
 # SPACE_DAMPING: a value of 0.9 means that each body will lose 10% of its velocity per second. Defaults to 1.
 LINEAR_FORCE = 100
 ANGULAR_VELOCITY = 0.3
